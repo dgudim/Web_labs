63d55260b46eef340479cd379464900635602a64
kloud <dgudim@gmail.com>
2021-10-26 19:51:19 +0300

    Task 10 DOM fix

--- Lab6/task10/script.js
+++ Lab6/task10/script.js
@@ -1,4 +1,4 @@
-setTimeout(function () {
+window.addEventListener('DOMContentLoaded', (event) => {
     let container = document.getElementById("container");
     let yacht = document.getElementById("yacht");
     let tower = document.getElementById("tower");
@@ -12,10 +12,7 @@ setTimeout(function () {
         setTimeout(function () {tower.classList.add("animate_sails");}, 300);
         setTimeout(function () {container.classList.add("animate_yacht");}, 900);
     }
-}, 1);//бред с загрузкой DOM
-// window.addEventListener('DOMContentLoaded', (event) => {
-    //console.log('DOM fully loaded and parsed')
-//});
+});
 
 
 
