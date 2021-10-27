144c69e22360c4d2e624965ab7896013498f89b6
kloud-linux-xps13 <dgudim@gmail.com>
2021-10-27 11:53:12 +0300

    Task 10 waves

--- Lab6/task10/script.js
+++ Lab6/task10/script.js
@@ -3,6 +3,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let yacht = document.getElementById("yacht");
     let tower = document.getElementById("tower");
 
+    let wave1 = document.getElementById("wave1");
+    let wave2 = document.getElementById("wave2");
+    let wave3 = document.getElementById("wave3");
+
     yacht.onclick = animate;
 
     function animate() {
@@ -10,7 +14,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
         yacht.classList.add("animate_borders");
         tower.classList.add("animate_tower");
         setTimeout(function () {tower.classList.add("animate_sails");}, 300);
-        setTimeout(function () {container.classList.add("animate_yacht");}, 900);
+        setTimeout(function () {wave1.classList.add("animate_wave");}, 400);
+        setTimeout(function () {wave2.classList.add("animate_wave");}, 500);
+        setTimeout(function () {wave3.classList.add("animate_wave");}, 600);
+        setTimeout(function () {container.classList.add("animate_yacht");}, 600);
     }
 });
 
